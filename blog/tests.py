32ec3425ea497194b982d5a5c242f5e11e844044
Alexey Kulichevskiy <alexey.kulichevskiy+alexchevsky@gmail.com>
2022-01-05 11:27:05 +0300

    added failing func tests for a blog

--- blog/tests.py
+++ blog/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from blog.views import home_page
 from django.http import HttpRequest
 
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
--- functional_tests.py
+++ functional_tests.py
@@ -14,27 +14,44 @@ class BasicInstallTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_home_page_title(self):  
+    def test_home_page_title(self):
         # В браузере Васи открылся сайт (по адресу http://127.0.0.1:8000)
         # В заголовке сайта Вася прочитал "Сайт Алексея Куличевского"
         self.browser.get('http://127.0.0.1:8000')
-        self.assertIn('Сайт Алексея Куличевского', self.browser.title)  
+        self.assertIn('Сайт Алексея Куличевского', self.browser.title)
 
-    def test_home_page_header(self):  
+    def test_home_page_header(self):
         # В шапке сайта написано "Алексей Куличевский"
         self.browser.get('http://127.0.0.1:8000')
         header = self.browser.find_element(By.TAG_NAME, 'h1')
         self.assertIn('Алексей Куличевский', header.text)
 
-if __name__ == '__main__':  
+    def test_home_page_blog(self):
+        # Под шапкой раположен блог со статьями.
+        self.browser.get('http://127.0.0.1:8000')
+        article_list = self.browser.find_element_by_class_name('article-list')
+        self.assertTrue(article_list)
+
+    def test_home_page_articles_look_correct(self):
+        # У каждой статьи есть заголовок и один абзац с текстом
+        self.browser.get('http://127.0.0.1:8000')
+        article_title = self.browser.find_element_by_class_name(
+            'article-title')
+        article_summary = self.browser.find_element_by_class_name(
+            'article-summary')
+        self.assertTrue(article_title)
+        self.assertTrue(article_summary)
+
+
+if __name__ == '__main__':
     unittest.main()
 
-# self.fail('Finish the test!')  
+# self.fail('Finish the test!')
 
 
-# Под шапкой раположен блог со статьями.
 
-# У каждой статьи есть заголовок и один абзац с текстом
+
+
 
 # Вася кликнул по заголовку и у него открылась страница с полным текстом статьи
 
