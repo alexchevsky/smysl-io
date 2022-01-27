10dde8953b54c54419588fec969ea4a7cebdd294
Alexey Kulichevskiy <alexey.kulichevskiy+alexchevsky@gmail.com>
2022-01-27 13:06:18 +0300

    added og_image to Article model

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     categery = models.CharField(max_length=255)
     pubdate = models.DateTimeField()
     slug = models.CharField(max_length=255, unique=True)
+    og_image = models.ImageField(upload_to='images', null=True)
     # is_published = models.BooleanField() #TODO
 
     def __str__(self):
