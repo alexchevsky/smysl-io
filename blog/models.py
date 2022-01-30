97591ef549fdbb126ba8ec7e58ffb28fc903fbc0
Alexey Kulichevskiy <alexey.kulichevskiy+alexchevsky@gmail.com>
2022-01-30 16:31:27 +0300

    Article summary is not a TextField

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    summary = models.CharField(max_length=255)
+    summary = models.TextField()
     full_text = models.TextField()
     category = models.CharField(max_length=255)
     pubdate = models.DateTimeField()
